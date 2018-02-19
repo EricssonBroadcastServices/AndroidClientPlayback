41f124a58bf105cb5c90789ea72ca9cd6a163441
João Coelho <admin.henyah@gmail.com>
2018-02-19 16:55:48 +0100

    Limiting seekbar to program duration (program-based timeline functionality).

--- src/main/java/net/ericsson/emovs/playback/techs/ExoPlayer/HookedDefaultTimeBar.java
+++ src/main/java/net/ericsson/emovs/playback/techs/ExoPlayer/HookedDefaultTimeBar.java
@@ -5,7 +5,9 @@ import android.util.AttributeSet;
 
 import com.google.android.exoplayer2.ui.DefaultTimeBar;
 
+import net.ericsson.emovs.utilities.interfaces.IEntitledPlayer;
 import net.ericsson.emovs.utilities.interfaces.IPlayer;
+import net.ericsson.emovs.utilities.models.EmpProgram;
 
 /**
  * Created by Joao Coelho on 2018-01-24.
@@ -14,6 +16,9 @@ import net.ericsson.emovs.utilities.interfaces.IPlayer;
 public class HookedDefaultTimeBar extends DefaultTimeBar {
     IPlayer player;
 
+    // TODO: update timeline when program changes
+    // TODO: hook timeline duration and current position
+    
     public HookedDefaultTimeBar(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -34,6 +39,15 @@ public class HookedDefaultTimeBar extends DefaultTimeBar {
 
     @Override
     public void setDuration(long duration) {
+        if (player instanceof IEntitledPlayer) {
+            IEntitledPlayer entitledPlayer = (IEntitledPlayer) player;
+            EmpProgram currentProgram = entitledPlayer.getCurrentProgram();
+            if (currentProgram != null && currentProgram.endDateTime != null && currentProgram.startDateTime != null) {
+                long programDuration = currentProgram.endDateTime.getMillis() - currentProgram.startDateTime.getMillis();
+                super.setDuration(programDuration);
+                return;
+            }
+        }
         super.setDuration(duration);
     }
 
