0c8f609a9cdcf7620e77f19fc17f04983ce2bbdf
João Coelho <admin.henyah@gmail.com>
2018-03-01 11:07:20 +0100

    More code cleanup.

--- src/test/java/net/ericsson/emovs/playback/helpers/FakeProgramService.java
+++ src/test/java/net/ericsson/emovs/playback/helpers/FakeProgramService.java
@@ -12,27 +12,5 @@ import net.ericsson.emovs.utilities.models.EmpProgram;
 public class FakeProgramService extends ProgramService {
     public FakeProgramService(IEntitledPlayer player, Entitlement entitlement, EmpProgram initialProgram) {
         super(player, entitlement, initialProgram);
-        this.eeCache = new FakeCache();
-    }
-
-    public void clear() {
-        ((FakeCache) eeCache).cacheCalled = false;
-    }
-
-    public boolean wasTimeAllowedCalled() {
-        return ((FakeCache) eeCache).cacheCalled;
-    }
-
-    private class FakeCache extends EntitlementCheckCache {
-        public boolean cacheCalled = false;
-
-        @Override
-        public Boolean isTimeAllowed(long playheadTime) {
-            Boolean outcome = super.isTimeAllowed(playheadTime);
-            if (outcome != null) {
-                cacheCalled = true;
-            }
-            return outcome;
-        }
     }
 }
